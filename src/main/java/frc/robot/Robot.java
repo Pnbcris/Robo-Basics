3ebbf0de8d8461af65be7fb4c31b531d9a674e7a
Pnbcris <cristian64470@gmail.com>
2024-07-11 14:38:39 -0700

    added basic XRP feature

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,26 +9,28 @@ import com.revrobotics.CANSparkMax; // MotorController
 import com.revrobotics.CANSparkLowLevel.MotorType; //Type of motor (switch between brush/brushless)
 import edu.wpi.first.wpilibj.PS4Controller; //Controller I am using (could be Xbox or PS)
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.xrp.XRPMotor; //XRPMotor
+import edu.wpi.first.wpilibj.xrp.XRPMotor;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
+
 public class Robot extends TimedRobot {
 private final CANSparkMax m_leftMotor = new CANSparkMax(7, MotorType.kBrushless);
 private final CANSparkMax m_rightMotor = new CANSparkMax(3, MotorType.kBrushed);
-// (m_) - basically means "mine"; its the name I give to the component
+
+private final XRPMotor m_XRPleftMotor = new XRPMotor(0);
+private final XRPMotor m_XRPrightMotor = new XRPMotor(1);
+
+
 private final XboxController m_NormalController = new XboxController(0); 
 private final PS4Controller m_SusController = new PS4Controller(1);
-private final XboxController m_ShooterController = new XboxController(2);
+private final XboxController m_XRPController = new XboxController(2);
 
-/*
-  "MotorType." - gets from import
-
-
-*/
+  //"MotorType." - gets from import
 
 
   @Override
@@ -45,12 +47,20 @@ private final XboxController m_ShooterController = new XboxController(2);
 
   @Override
   public void teleopInit() {
-
+    //occurs only when robot is in teleop mode
+    m_XRPrightMotor.setInverted(true);
   }
 
   @Override
   public void teleopPeriodic() {
-    if (m_NormalController.getAButton()) //"get" means its grabbing something from controller
+    double forward = -m_XRPController.getLeftY();
+    double turn = -m_XRPController.getRightY();
+
+    m_XRPleftMotor.set(forward - turn);
+    m_XRPrightMotor.set(forward + turn);
+
+
+   /* if (m_NormalController.getAButton()) //"get" means its grabbing something from controller
     {
       m_leftMotor.set(.15);
     }
@@ -75,6 +85,7 @@ private final XboxController m_ShooterController = new XboxController(2);
     {
       m_rightMotor.set(0);
     }
+    */
   }
 
 
